edb1fff024a1699ca56557bd65cf6f767ea9499d
Metal Face <50063043+metalfacesec@users.noreply.github.com>
2021-12-23 00:08:35 -0500

    Pull all game pages

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const Igdb = require(`${__dirname}/utils/Igdb`);
 const MySql = require(`${__dirname}/utils/MySql`);
+const Utils = require(`${__dirname}/utils/Utils`);
 
 const config = require(`${__dirname}/../config/config.json`);
 
@@ -12,10 +13,23 @@ async function scrapeData() {
         MySql.runQuery(config, MySql.getPlatformsInsertQuery(platforms));
         console.log('Platform db updated');
 
-        let games = await igdb.getGamesPage();
-        await MySql.runQuery(config, MySql.getGamesInsertQuery(games));
-        await MySql.runQuery(config, MySql.getGamesPlatformsInsertQuery(games));
-        console.log('Games db page updated');
+        let offset = 0;
+        while (true) {
+            let games = await igdb.getGamesPage(offset);
+
+            if (!Array.isArray(games) || !games.length) {
+                break;
+            }
+
+            await MySql.runQuery(config, MySql.getGamesInsertQuery(games));
+            await MySql.runQuery(config, MySql.getGamesPlatformsInsertQuery(games));
+            console.log('Games db page updated');
+
+            offset += 100;
+            Utils.sleep(500);
+        }
+
+        console.log('All games updated');
     } catch (error) {
         console.log(error);
     }
